0172e9a99e773d99861903e90b21bf2cc14d2ca4
V-B-Bozhinskiy <bove203@outlook.com>
2021-12-04 10:22:59 +0300

    Создание файла для изучения условий

---
+++ conditions.php
@@ -0,0 +1,17 @@
+<?php
+$number = 10;
+
+if ($number > 5){
+    echo "You Passed";
+    echo '<br>';
+} 
+echo "Эта строка выводится всегда! <br>";
+
+if ($number > 5){
+    echo "You Passed";
+    echo '<br>';
+} else {
+    echo "Stydy again and try again";
+    echo '<br>';
+}
+echo "Эта строка выводится всегда! <br>";
