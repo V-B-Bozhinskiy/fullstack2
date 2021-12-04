601c9b178e1e6d182cfee5dc62a5a655a8a7cc80
V-B-Bozhinskiy <bove203@outlook.com>
2021-12-04 10:56:51 +0300

    Дополнение кода

--- conditions.php
+++ conditions.php
@@ -2,16 +2,36 @@
 $number = 10;
 
 if ($number > 5){
-    echo "You Passed";
+    echo "Число больше 5";
     echo '<br>';
 } 
 echo "Эта строка выводится всегда! <br>";
 
 if ($number > 5){
-    echo "You Passed";
+    echo "Число больше 5";
     echo '<br>';
 } else {
-    echo "Stydy again and try again";
+    echo "Число иеньше или равно 5";
+    echo '<br>';
+} 
+echo "Эта строка выводится всегда! <br>";
+
+if ($number > 5){
+    echo "Число больше 5";
+    echo '<br>';
+} else if ($number > 3){
+    echo "Число меньше или равно 5, но больше 3";
+    echo '<br>';
+} else if ($number > 0){
+    echo "Либо 1, либо 2, либо 3";
+    echo '<br>';
+} else {
+    echo "0 или отрицательное";
     echo '<br>';
 }
-echo "Эта строка выводится всегда! <br>";
+
+$second_number = 0;
+
+if ($second_number != 0){
+    echo "$second_number != 0";
+}
