71a5d317c17227061a27d495a21654372dac205b
V-B-Bozhinskiy <bove203@outlook.com>
2021-12-16 18:47:51 +0300

    Перенос файлов

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
 <?php
-echo "Hello, World";
-echo '<br>';
-echo 'Hello, PHP';
-echo '<br>';
-echo 55;
-echo '<br>';
-echo date('d.m.Y H:i:s');
-echo '<br>';
-echo 2 + 1;
-echo '<br>';
-echo 2 * 3 -5;
-echo '<br>';
-echo 2 ** 6;
-echo '<br>';
-echo 10 % 3;
