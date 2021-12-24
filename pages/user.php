0d5ba09d78bc03333e27376953858e3fb95cf725
V-B-Bozhinskiy <bove203@outlook.com>
2021-12-24 12:30:51 +0300

    Добавлено исчезание сообщения об изменении данных через 3 секунды

--- pages/user.php
+++ pages/user.php
@@ -8,6 +8,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
 </head>
   <body>
     <div class='container mt-5'>
@@ -46,7 +50,7 @@ if ($user){
 
    if (isset($_SESSION['result'])){
     echo "
-    <div class='alert alert-success text-center' role='alert'>
+    <div id='alertSuccess' class='alert alert-success text-center' role='alert'>
          {$_SESSION['result']}
     </div>
     ";
@@ -73,6 +77,13 @@ if ($user){
         <button type='submit' class="btn btn-success w-100"> СОХРАНИТЬ </button>
     </form>
     </div>
+    <script> 
+        $(document).ready(function(){
+            setTimeout(function(){
+                $('#alertSuccess').fadeOut()
+            }, 3000)
+        })
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
