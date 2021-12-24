4272b189f37bae593ffe6d2a48ca64cd37bb0e57
V-B-Bozhinskiy <bove203@outlook.com>
2021-12-24 12:33:22 +0300

    Убран закомментированный код

--- actions/register.php
+++ actions/register.php
@@ -25,51 +25,6 @@ $users = $pdo->query($query);
 $query = "SELECT * FROM cities";
 $cities = $pdo->query($query);
 
-
-//print_r($res->fetchAll());
-/*
-echo("
-    <table class = 'table table-bordered'>
-    <thead>
-        <tr>
-            <td> id </td>
-            <td> Логин </td>
-            <td> Имя </td>
-           <td> id города </td>
-        </tr>
-    </thead>
-    <tbody>
-");
-
-foreach ($users as $user){
-    $city = $user['city_id'] ? $user['city_id'] : '-';
-    
-    echo "
-        <tr>
-            <td>{$user['id']}</td>
-            <td>{$user['login']}</td>
-            <td>
-            <a href = 'pages/user.php?id={$user['id']}'>
-                {$user['name']}
-            </a>
-            </td>
-            <td>{$city}</td>
-            <td class='text-center'>
-                <form method='post' action='actions/del_user.php'>
-                    <input hidden name='id' value='{$user['id']}'>
-                    <button class='btn btn-danger'>x</button>
-                </form>
-            </td>
-        </tr>
-    ";
-
-
-}
-
-echo "</tbody>
-     </table>   ";
-?>
-*/
 ?>
  <form method="POST" action="../form.php">
             <input required class="form-control mb-2" placeholder="Имя" name='name'>
