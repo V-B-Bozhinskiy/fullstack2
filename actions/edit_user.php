82df146b8a1802286b7a6527af2162dd6c19822b
V-B-Bozhinskiy <bove203@outlook.com>
2021-12-24 11:13:46 +0300

    fix forget city save

--- actions/edit_user.php
+++ actions/edit_user.php
@@ -3,18 +3,20 @@ session_start();
 $userId = $_POST['id'];
 $name = $_POST['name'];
 $login = $_POST['login'];
+$city_id = $_POST['city_id'];
 
 $user = 'root';
 $password = '';
 
 $pdo = new Pdo('mysql:dbname=fullstack2;host=127.0.0.1',$user,$password);
 
-$query = "UPDATE users SET name = :name, login = :login WHERE id = :id";
+$query = "UPDATE users SET name = :name, login = :login, city_id = :city_id WHERE id = :id";
 $res = $pdo->prepare($query);
 $status = $res->execute([
     ':id' => $userId,
     ':login' => $login,
-    ':name' => $name
+    ':name' => $name,
+    ':city_id' => $city_id
 ]);
 
 if (!$status){
